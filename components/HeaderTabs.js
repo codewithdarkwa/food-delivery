5282fce5d259b2936b4278a89cc94b49d8dcc9af
codewithdarkwa <jnrdrkw1@gmail.com>
2022-07-13 07:15:15 +0000

    dependencies added

---
+++ components/HeaderTabs.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function HeaderTabs(){
+    return(
+        <View>
+            <Text>Header Tabs</Text>
+        </View>
+    )
+}
---
+++ screens/Home.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function HeaderTabs(){
+    return(
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    )
+}
