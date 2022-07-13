5e60581571c834609f1aba8a97c359231bed280f
codewithdarkwa <jnrdrkw1@gmail.com>
2022-07-13 08:42:20 +0000

    categories done

--- screens/Home.js
+++ screens/Home.js
@@ -10,7 +10,7 @@ export default function Home(){
            <HeaderTabs />
            <SearchBar />
             </View>
-          <Categories />
+          <Categories/>
         </SafeAreaView>
     )
 }
