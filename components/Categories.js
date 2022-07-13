31239364313469d1fb963ce7a1f6d43d8fd8ae6b
codewithdarkwa <jnrdrkw1@gmail.com>
2022-07-13 08:19:22 +0000

    done with the searchbar component

---
+++ components/Categories.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native'
+
+
+export default function Categories(){
+    return(
+        <View>
+            <Text>Cat</Text>
+        </View>
+    )
+}
--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,16 @@
 import { View, Text,SafeAreaView } from 'react-native'
+import Categories from '../components/Categories'
 import HeaderTabs from '../components/HeaderTabs'
+import SearchBar from '../components/SearchBar'
 
 export default function Home(){
     return(
         <SafeAreaView style={{backgroundColor:'#eee',flex: 1}}>
             <View style={{backgroundColor:'#fff',padding:15}}>
            <HeaderTabs />
+           <SearchBar />
             </View>
+          <Categories />
         </SafeAreaView>
     )
 }
