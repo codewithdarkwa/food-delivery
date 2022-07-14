2ba5d0fad039b7fcfe714be6d1a18c3957d19a5c
codewithdarkwa <jnrdrkw1@gmail.com>
2022-07-14 17:25:04 +0000

    added a view cart button

--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -2,17 +2,18 @@ import {View, Text} from 'react-native'
 import { Divider } from 'react-native-elements/dist/divider/Divider'
 import About from '../components/RestaurantDetail/About'
 import MenuItem from '../components/RestaurantDetail/MenuItem'
+import ViewCart from '../components/RestaurantDetail/ViewCart'
 
 
 
-
-export default function RestaurantDetail({route}){
+export default function RestaurantDetail({route,navigation}){
 
     return(
         <View>
            <About route={route}/>
            <Divider width={1.8} style={{marginVertical:20}}/>
            <MenuItem />
+           <ViewCart navigation={navigation} retaurantName={route.params.name}/>
         </View>
     )
 }
