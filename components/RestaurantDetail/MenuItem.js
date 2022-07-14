12d6322934a189c3dbfce7ddd11c7681f494ab09
codewithdarkwa <jnrdrkw1@gmail.com>
2022-07-14 16:59:48 +0000

    added a checkbox

--- components/RestaurantDetail/MenuItem.js
+++ components/RestaurantDetail/MenuItem.js
@@ -1,6 +1,6 @@
 import {View, Text,StyleSheet,Image,ScrollView} from 'react-native'
 import { Divider } from 'react-native-elements/dist/divider/Divider'
-
+import BouncyCheckbox from 'react-native-bouncy-checkbox'
 
 const food = [
     {
@@ -46,6 +46,10 @@ export default function MenuItem(){
         {food.map((food,index)=>(
         <View key={index}>
         <View style={styles.menuStyle}>
+        <BouncyCheckbox 
+        iconStyle={{borderColor:'lightgray',borderRadius:0}}
+        fillColor='green'
+        />
            <FoodInfo food={food}/>
            <FoodImage food={food}/>
         </View>
