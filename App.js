aade1f0bda9cc66113f5b93e62a995f74ff2cb52
codewithdarkwa <jnrdrkw1@gmail.com>
2022-07-16 22:24:08 +0000

    added status bar

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import RootNavigation from './navigation';
 
 export default function App() {
   return (
-   <RootNavigation />
+    <>
+    <RootNavigation />
+    <StatusBar style='dark'/>
+    </>
   );
 }
 
